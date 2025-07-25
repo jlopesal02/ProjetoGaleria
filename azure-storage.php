f2533834be0b17509ed42d280f84b79057568c19
João Pedro Alves <123829163+jlopesal02@users.noreply.github.com>
2025-07-25 19:53:37 +0100

    Add files via upload

--- azure-storage.php
+++ azure-storage.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 
 // Lê variáveis de ambiente definidas no ambiente da App
-$accountName = getenv('AZURE_STORAGE_ACCOUNT');
-$accountKey  = getenv('AZURE_STORAGE_KEY');
+$accountName = getenv('joaoalves30394');
+$accountKey  = getenv('J1ON1LVKngZQ1OS9JY3wnSL6wFexQYFJUNQRKuXa3ZXNNEbTu8n6bzl1SurOZRWrt9TjXg0E4POK+ASt0CRitg==');
 
 if (!$accountName || !$accountKey) {
     die("Erro: variáveis de ambiente AZURE_STORAGE_ACCOUNT ou AZURE_STORAGE_KEY não definidas.");
